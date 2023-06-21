0abe9845c7eba60987c6d3adfc6f6335ba120317
Thorsten <ts@taurus.scrabblenet.local>
2023-06-21 21:15:48 +0200

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,16 @@
+
+import { handleSubmit } from './js/formHandler'
+import { queryWeb } from './js/queryWeb'
+import { uploadTxt } from './js/uploadTxt'
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+import './styles/header.scss'
+
+
+
+export {
+    handleSubmit, uploadTxt, queryWeb
+}
