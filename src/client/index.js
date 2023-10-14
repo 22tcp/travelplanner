893097850b0a1045f1f2f30ac758c0132b1bb79e
Thorsten <tschocke@gmx.de>
2023-10-14 20:36:38 +0200

    client index.js crete date input all css - reformat for input area,grid downsize index.html adding IDs, div restructure

--- src/client/index.js
+++ src/client/index.js
@@ -18,6 +18,22 @@ function addPicture() {
 
 document.getElementById('vPicture').appendChild(addPicture());
 
+//calculate offset for min input and create the date picker with it
+
+
+function addDateInput() {
+  let dobject = new Date( new Date().getTime());
+  const tzo = dobject.getTimezoneOffset();
+  dobject.setDate(dobject.getDate() + 1)
+  let dlimit = dobject.toISOString().slice(0,10);
+  const element = document.createElement('input');
+  element.setAttribute("type", "date")
+  element.setAttribute("min", dlimit );
+  element.setAttribute("max","2099-12-31");
+  return element;
+}
+document.getElementById('datepicker').appendChild(addDateInput());
+
 document.addEventListener('DOMContentLoaded', () => {
     // Add event listener to a button or any other element
     const button = document.getElementById('myButton');
