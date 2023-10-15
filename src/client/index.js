651ee7c3fc62dac3e2b6722adc39077ae812ce43
Thorsten <tschocke@gmx.de>
2023-10-15 23:01:05 +0200

    client index.js eventListener index.html   input destination

--- src/client/index.js
+++ src/client/index.js
@@ -23,10 +23,10 @@ document.getElementById('vPicture').appendChild(addPicture());
 function addDateInput() {
   let dobject = new Date( new Date().getTime());
   const tzo = dobject.getTimezoneOffset();
-  dobject.setDate(dobject.getDate() + 1)
+  dobject.setDate(dobject.getDate() + 1);
   let dlimit = dobject.toISOString().slice(0,10);
   const element = document.createElement('input');
-  element.setAttribute("type", "date")
+  element.setAttribute("type", "date");
   element.setAttribute("min", dlimit );
   element.setAttribute("max","2099-12-31");
   element.classList.add("date-picker");
@@ -34,10 +34,22 @@ function addDateInput() {
 }
 document.getElementById('datepicker').appendChild(addDateInput());
 
+
+
 document.addEventListener('DOMContentLoaded', () => {
     // Add event listener to a button or any other element
     const button = document.getElementById('myButton');
     if ( button == typeof(Object) ) { button.addEventListener('click', handleSubmit);}
+
+    document.getElementById('destination').addEventListener(
+      "keyup", (ev) => { 
+        if ( ev.key === 'Enter' || ev.code === 13 ) {
+          ev.preventDefault;
+        }
+      }
+    );
+
+    //document.getElementById('voyageAdd').addEventListener('click', checkCompleteness )
   });
 
   
