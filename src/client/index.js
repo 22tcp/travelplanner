111dded3bd547f8f7cda5e1e6cf6e2261a063f54
Thorsten <tschocke@gmx.de>
2023-10-23 01:36:39 +0200

    shifting functions from index.js to app.js adding localStorage save

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,9 @@ import './styles/main.scss'
 import Pic from '../static/travelcat.jpg'
 //import * as bootstrap from 'bootstrap'
 
-import { handleSubmit  } from './js/app.js'
+import { handleSubmit } from './js/app.js'
+import { checkCompleteness } from './js/app.js'
+import { addDateInput } from './js/app.js'
 
 export { handleSubmit }
 
@@ -11,63 +13,28 @@ function addPicture() {
   const myImage = new Image();
   myImage.classList.add("ha-image");
   myImage.src = Pic;
-  myImage.id  = 'Pic';
+  myImage.id = 'Pic';
   return myImage;
 }
 
-document.getElementById('vPicture').appendChild(addPicture());
-
-//calculate offset for min input and create the date picker with it
 
 
-function addDateInput() {
-  let dobject = new Date( new Date().getTime());
-  const tzo = dobject.getTimezoneOffset();
-  dobject.setDate(dobject.getDate() + 1);
-  let dlimit = dobject.toISOString().slice(0,10);
-  const element = document.createElement('input');
-  element.setAttribute("type", "date");
-  element.setAttribute("min", dlimit );
-  element.setAttribute("max","2099-12-31");
-  element.setAttribute("id", "datepickerinput")
-  element.classList.add("date-picker");
-  return element;
-}
-document.getElementById('datepicker').appendChild(addDateInput());
 
-const checkCompleteness = () => {
-  const destdata = document.getElementById('destination');
-  const lengthdestdata = destdata.value.length;
-  const lengthdatedata = document.getElementById('datepickerinput').value.length;
-  const incompobj = document.getElementById('incomplete');
-  const duration  = document.getElementById('duration');
-  if ( lengthdestdata < 1 || lengthdatedata << 1 ){
-    incompobj.classList.remove('nodisplay');
-    destdata.focus();
-    incompobj.classList.add('important');
-    duration.classList.add('nodisplay');
-
-    setTimeout( () => {
-      incompobj.classList.add('nodisplay');
-      duration.classList.remove('nodisplay');
-    }, 4000);
-    return;
-  }
-}
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Add event listener to a button or any other element
-    const button = document.getElementById('myButton');
-    if ( button == typeof(Object) ) { button.addEventListener('click', handleSubmit);}
+  // Add event listener to a button or any other element
+  const button = document.getElementById('myButton');
+  if (button == typeof (Object)) { button.addEventListener('click', handleSubmit); }
 
-    document.getElementById('destination').addEventListener(
-      "keyup", (ev) => { 
-        if ( ev.key === 'Enter' || ev.code === 13 ) {
-          ev.preventDefault;
-        }
+  document.getElementById('destination').addEventListener(
+    "keyup", (ev) => {
+      if (ev.key === 'Enter' || ev.code === 13) {
+        ev.preventDefault;
       }
-    );
-
-    document.getElementById('voyageAdd').addEventListener('click', checkCompleteness )
+    }
+  );
+  document.getElementById('vPicture').appendChild(addPicture());
+  document.getElementById('datepicker').appendChild(addDateInput());
+  document.getElementById('voyageAdd').addEventListener('click', checkCompleteness);
 });
 
--- src/client/js/app.js
+++ src/client/js/app.js
@@ -38,3 +38,44 @@ export const uploadTo = async (url = '', data = {}) => {
 
 }
 
+//calculate offset for min input and create the date picker with it
+export const checkCompleteness = () => {
+  const destdata = document.getElementById('destination');
+  const datedata = document.getElementById('datepickerinput')
+  const lengthdestdata = destdata.value.length;
+  const lengthdatedata = datedata.value.length;
+  const incompobj = document.getElementById('incomplete');
+  const duration  = document.getElementById('duration');
+  if ( lengthdestdata < 1 || lengthdatedata <  1 ){
+    console.log(lengthdestdata + " lenght dest")
+    console.log(lengthdatedata + " length date")
+    incompobj.classList.remove('nodisplay');
+    destdata.focus();
+    incompobj.classList.add('important');
+    duration.classList.add('nodisplay');
+
+    return(setTimeout( () => {
+      incompobj.classList.add('nodisplay');
+      duration.classList.remove('nodisplay');
+    }, 4000));
+    
+  } else {
+    console.log("data complete, storing ")
+    localStorage.setItem("travelapp_destination", destdata.value )
+    localStorage.setItem("travelapp.date", datedata.value )
+  }
+}
+
+export function addDateInput() {
+  let dobject = new Date( new Date().getTime());
+  const tzo = dobject.getTimezoneOffset();
+  dobject.setDate(dobject.getDate() + 1);
+  let dlimit = dobject.toISOString().slice(0,10);
+  const element = document.createElement('input');
+  element.setAttribute("type", "date");
+  element.setAttribute("min", dlimit );
+  element.setAttribute("max","2099-12-31");
+  element.setAttribute("id", "datepickerinput")
+  element.classList.add("date-picker");
+  return element;
+}
